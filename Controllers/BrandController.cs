b422442b3c3430add5ed6627e114b710469ee38b
Василь Пелешко <105241875+luff3@users.noreply.github.com>
2023-11-13 21:56:27 +0200

    getRequest

---
+++ Controllers/BrandController.cs
@@ -0,0 +1,54 @@
+﻿using lab04.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace lab04.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandController : ControllerBase
+    {
+        private readonly ShopContext _shopContext;
+
+        public BrandController(ShopContext shopContext)
+        {
+            _shopContext = shopContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Brand>>> GetBrands()
+        {
+            if(_shopContext.Brands == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return await _shopContext.Brands.ToListAsync();
+            }
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<Brand>> GetBrand(int id)
+        {
+            if (_shopContext.Brands == null)
+            {
+                return NotFound();
+            }
+            
+            var brand = await _shopContext.Brands.FindAsync(id);
+
+            if(brand == null) 
+            {
+                return NotFound();
+            }
+            else
+            {
+                return brand;
+            }
+        }
+    }
+}
