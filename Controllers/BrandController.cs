ef251406a5256ae40a74b2b6d2f7eab93f955bba
Василь Пелешко <105241875+luff3@users.noreply.github.com>
2023-11-13 23:20:39 +0200

    One completed controller

--- Controllers/BrandController.cs
+++ Controllers/BrandController.cs
@@ -31,7 +31,7 @@ namespace lab04.Controllers
         }
 
 
-        [HttpGet]
+        [HttpGet("{id}")]
         public async Task<ActionResult<Brand>> GetBrand(int id)
         {
             if (_shopContext.Brands == null)
@@ -50,5 +50,67 @@ namespace lab04.Controllers
                 return brand;
             }
         }
+
+        [HttpPost]
+        public async Task<ActionResult<Brand>> PostBrand(Brand brand)
+        {
+            _shopContext.Brands.Add(brand); 
+            _shopContext.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetBrand), new {id = brand.ID}, brand);
+        }
+        [HttpPut]
+        public async Task<IActionResult> PutBrand(int id, Brand brand)
+        {
+            if(id != brand.ID)
+            {
+                return BadRequest();
+            }
+            _shopContext.Entry(brand).State = EntityState.Modified;
+
+            try
+            {
+                await _shopContext.SaveChangesAsync();
+            }
+            catch(DbUpdateConcurrencyException) 
+            { 
+                if(!BrandAvailable(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            
+            }
+            return Ok();
+        }
+
+        private bool BrandAvailable(int id)
+        {
+            return (_shopContext.Brands?.Any(x => x.ID == id)).GetValueOrDefault();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteBrand(int id)
+        {
+            if(_shopContext.Brands == null)
+            {
+                return NotFound();
+            }
+            var brand = await _shopContext.Brands.FindAsync(id);
+
+            if(brand == null)
+            {
+                return NotFound();
+            }
+            _shopContext.Brands.Remove(brand);
+            await _shopContext.SaveChangesAsync();
+            return Ok();
+
+        }
+
+
     }
 }
