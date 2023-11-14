44dcf8f01ba418ecdabd239051ab53631d5590ce
Василь Пелешко <105241875+luff3@users.noreply.github.com>
2023-11-14 13:53:33 +0200

    Done all Controllers

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -2,72 +2,72 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using WebApplication1.Models;
 
 namespace lab04.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class EmployeeController : ControllerBase
+    public class CustomerController : ControllerBase
     {
         private readonly ShopContext _shopContext;
-        public EmployeeController(ShopContext shopContext)
+
+        public CustomerController(ShopContext shopContext)
         {
             _shopContext = shopContext;
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
+        public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers()
         {
-            if (_shopContext.Brands == null)
+            if (_shopContext.Customers == null)
             {
                 return NotFound();
             }
             else
             {
-                return await _shopContext.Employees.ToListAsync();
+                return await _shopContext.Customers.ToListAsync();
             }
         }
 
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Employee>> GetEmployee(int id)
+        public async Task<ActionResult<Customer>> GetCustomer(int id)
         {
-            if (_shopContext.Employees == null)
+            if (_shopContext.Customers == null)
             {
                 return NotFound();
             }
 
-            var employee = await _shopContext.Employees.FindAsync(id);
+            var customer = await _shopContext.Customers.FindAsync(id);
 
-            if (employee == null)
+            if (customer == null)
             {
                 return NotFound();
             }
             else
             {
-                return employee;
+                return customer;
             }
         }
 
         [HttpPost]
-        public async Task<ActionResult<Employee>> PostEmployee(Employee employee)
+        public async Task<ActionResult<Customer>> PostEmployee(Customer customer)
         {
-            _shopContext.Employees.Add(employee);
+            _shopContext.Customers.Add(customer);
             await _shopContext.SaveChangesAsync();
 
-            return CreatedAtAction(nameof(GetEmployee), new { id = employee.ID }, employee);
+            return CreatedAtAction(nameof(GetCustomer), new { id = customer.ID }, customer);
         }
 
 
         [HttpPut]
-        public async Task<IActionResult> PutEmployee(int id, Employee employee)
+        public async Task<IActionResult> PutEmployee(int id, Customer customer)
         {
-            if (id != employee.ID)
+            if (id != customer.ID)
             {
                 return BadRequest();
             }
-            _shopContext.Entry(employee).State = EntityState.Modified;
+            _shopContext.Entry(customer).State = EntityState.Modified;
 
             try
             {
@@ -75,7 +75,7 @@ namespace lab04.Controllers
             }
             catch (DbUpdateConcurrencyException)
             {
-                if (!EmployeeAvailable(id))
+                if (!CustomerAvailable(id))
                 {
                     return NotFound();
                 }
@@ -88,31 +88,29 @@ namespace lab04.Controllers
             return Ok();
         }
 
-        private bool EmployeeAvailable(int id)
+        private bool CustomerAvailable(int id)
         {
-            return (_shopContext.Employees?.Any(x => x.ID == id)).GetValueOrDefault();
+            return (_shopContext.Customers?.Any(x => x.ID == id)).GetValueOrDefault();
         }
 
         [HttpDelete("{id}")]
-        public async Task<ActionResult> DeleteEmployee(int id)
+        public async Task<ActionResult> DeleteCustomer(int id)
         {
-            if (_shopContext.Employees == null)
+            if (_shopContext.Customers == null)
             {
                 return NotFound();
             }
-            var employee = await _shopContext.Employees.FindAsync(id);
+            var customer = await _shopContext.Customers.FindAsync(id);
 
-            if (employee == null)
+            if (customer == null)
             {
                 return NotFound();
             }
-            _shopContext.Employees.Remove(employee);
+            _shopContext.Customers.Remove(customer);
             await _shopContext.SaveChangesAsync();
             return Ok();
 
         }
 
-
     }
 }
-
